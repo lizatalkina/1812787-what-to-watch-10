f24bb6d5d3142faaef2eb1a837ff41db8baf2c70
lizatalkina <liza.talkina@gmail.com>
2022-07-10 18:06:55 +0300

    Создает компоненты страниц

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainScreen from '../../pages/main-screen/main-screen';
+
+type AppMainFilmProps = {
+  filmTitle: string;
+  filmGenre: string;
+  filmYear: number;
+}
+
+function App({filmTitle, filmGenre, filmYear}: AppMainFilmProps): JSX.Element {
+  return <MainScreen filmTitle={filmTitle} filmGenre={filmGenre} filmYear={filmYear} />;
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const MainMovie = {
+  movieTitle: 'The Grand Budapest Hotel',
+  movieGenre: 'Drama',
+  movieYear: 2014,
+};
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      filmTitle = {MainMovie.movieTitle}
+      filmGenre = {MainMovie.movieGenre}
+      filmYear = {MainMovie.movieYear}
+    />
   </React.StrictMode>,
 );
